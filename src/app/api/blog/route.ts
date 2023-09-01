f855107043beadd0748f58e737ae5e2b826a47b5
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-09-01 15:12:46 +0900

    全記事取得と投稿用API

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+
+const prisma = new PrismaClient();
+
+export async function main() {
+    try {
+        await prisma.$connect();
+    } catch (error) {
+        return Error("DB接続に失敗しました");
+    }
+}
+
+
+// ブログの全記事取得用API
+export const GET = async (req:Request,res:NextResponse) => {
+try{
+    await main();
+    const posts = await prisma.post.findMany();
+    return NextResponse.json({message:"Success",posts},{status:200});
+}catch(err){
+    return NextResponse.json({message:"Error",err},{status:500});
+}finally{
+    await prisma.$disconnect();
+}
+};
+
+
+
+// ブログ投稿用API
+export const POST = async (req:Request,res:NextResponse) => {
+    try{
+
+        const {title, description} = await req.json();
+
+        await main();
+        const post = await prisma.post.create({data:{title, description}});
+        // ここのtitleとdescriptionはrequestの中に入っているのでtry文の中のawait main()の上に
+        return NextResponse.json({message:"Success",post},{status:201});
+    }catch(err){
+        return NextResponse.json({message:"Error",err},{status:500});
+    }finally{
+        await prisma.$disconnect();
+    }
+    };
